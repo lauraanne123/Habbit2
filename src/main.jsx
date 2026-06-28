ff55da17c7279051d6fc658292147b8e508031bb
lauraanne123 <lauraanne123@gmail.com>
2026-06-28 15:50:53 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import HabitTracker from "./App";
+
+createRoot(document.getElementById("root")).render(<HabitTracker />);
